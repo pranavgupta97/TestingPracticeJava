008697792e99ced693ac3dcef3256397447c5aed
Pranav Gupta <pranavgupta9797@gmail.com>
2020-01-04 14:38:15 -0600

    Added first test for Helper class

--- src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,19 @@
 package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-	
+    @Test
+    public void testOne() {
+        List<String> employeeNames = Arrays.asList("Pranav", "Bruh");
+        long actualCountBruh = Helper.getCount(employeeNames);
+        assertEquals(2, actualCountBruh);
+    }
 }
