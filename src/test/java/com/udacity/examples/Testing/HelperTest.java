ff3d8b5745d692f533a646c05accdd0813ee5f58
Pranav Gupta <pranavgupta9797@gmail.com>
2020-01-04 15:06:09 -0600

    Added test for GetStats in Helper Class

--- src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -15,5 +16,16 @@ public class HelperTest {
         List<String> employeeNames = Arrays.asList("Pranav", "Bruh");
         long actualCountBruh = Helper.getCount(employeeNames);
         assertEquals(2, actualCountBruh);
+    }
+
+    @Test
+    public void testGetStats() {
+        List<Integer> yrsOfExperience = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        List<Integer> actualYearsOfExperience = Arrays.asList(13, 4, 15, 6, 17, 8, 19, 1, 2, 3);
+        IntSummaryStatistics statistics = Helper.getStats(yrsOfExperience);
+
+        assertEquals(19, statistics.getMax());
+
+        assertEquals(yrsOfExperience, actualYearsOfExperience);
     }
 }
